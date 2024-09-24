f102172b87acb1ed61d3be696deee8673858cd16
Ujjwal Shekhawat <ujjus2001@gmail.com>
2024-09-24 14:42:47 +0530

    update: dockercompose

--- user-service/config/config.go
+++ user-service/config/config.go
@@ -3,11 +3,14 @@ package config
 import "os"
 
 type ServiceConfig struct {
-	ServerPort string
+	ServerPort       string
+	CassandraCluster string
 }
 
 func LoadConfig() *ServiceConfig {
-	return &ServiceConfig{ServerPort: getEnvOrDefault("SERVICE_PORT", ":5051")}
+	return &ServiceConfig{
+		ServerPort:       getEnvOrDefault("SERVICE_PORT", ":5051"),
+		CassandraCluster: getEnvOrDefault("CASSANDRA_CLUSTER", "localhost:9042,localhost:9043,localhost:9044")}
 }
 
 func getEnvOrDefault(key, defaultValue string) string {
